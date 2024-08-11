98756e3d172cbe38c68b345369b4f838db918d95
oldnick85 <nekit_nt@mail.ru>
2024-08-11 15:16:08 +0300

    [add] use ygg 0.5.7 by default

--- build_image.sh
+++ build_image.sh
@@ -2,7 +2,7 @@
 TAG_VERSION=$(git describe --tags || echo "unknown")
 docker build \
     --build-arg UBUNTU_VERSION=24.04 \
-    --build-arg YGGDRASIL_VERSION=v0.5.6 \
+    --build-arg YGGDRASIL_VERSION=v0.5.7 \
     --tag tor_yggdrasil:${TAG_VERSION} \
     --tag tor_yggdrasil:latest \
     .
